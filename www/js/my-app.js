869264824fe3b6ffd093a30c4b28265c9bca0f9b
ravenard <demonrus99@gmail.com>
2016-12-07 13:44:47 +1000

    cache

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,16 +1,11 @@
-// Initialize app
+
 var myApp = new Framework7();
 
 var globalVar = {};
 
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $ = Dom7;
 
-//alert('1');
-
-		
 //Укороченный ajax.
-		
 var ajax = function(url, data, onSuccess){
 	$.ajax({
 		method : 'POST',
